597a5b956979dee446d52542a334ce43130144fe
pujie1216 <21360598+pujie1216@users.noreply.github.com>
2021-04-02 19:50:50 +0800

    添加安装某些新模块命令

--- 安装模块.bat
+++ 安装模块.bat
@@ -4,4 +4,6 @@ title 安装额外模块
 python -m ensurepip
 python -m pip install requests
 python -m pip install rsa
+python -m pip install qrcode
+python -m pip install Pillow
 echo.&echo 没有红色错误就按任意键完成安装&pause>nul
